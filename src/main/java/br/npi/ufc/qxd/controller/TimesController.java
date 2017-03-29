594d44cfde6004979f8e12b0932061bf65249f70
northEXE <wendel.ferreira.wfm@hotmail.com>
2017-03-29 10:02:20 -0300

    Adição da tela detalhes time, modificação das telas anteriores e modificação do TimeController

--- src/main/java/br/npi/ufc/qxd/controller/TimesController.java
+++ src/main/java/br/npi/ufc/qxd/controller/TimesController.java
@@ -1,6 +1,7 @@
 package br.npi.ufc.qxd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,10 @@ public class TimesController {
 	@RequestMapping(path="/")
 	public String index(){
 		return "times";
+	}
+	
+	@RequestMapping(path="{id}")
+	public String detalhesTime(@PathVariable Integer id){
+		return "detalhestime";
 	}
 }
