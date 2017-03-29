9edef73d85f288d7eda59d6ba0933c7d4207f0a1
northEXE <wendel.ferreira.wfm@hotmail.com>
2017-03-29 09:26:02 -0300

    Adicção dos getters e setters da bean Jogador

--- src/main/java/br/npi/ufc/qxd/bean/Jogador.java
+++ src/main/java/br/npi/ufc/qxd/bean/Jogador.java
@@ -17,4 +17,38 @@ public class Jogador {
 	
 	@OneToOne
 	Time time;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Time getTime() {
+		return time;
+	}
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	
+	
 }
